1abd9d81134ff64e00423b46ede9e461a1adbd7b
thanh <thanh@MSI>
2025-04-13 05:55:21 -0800

    Revert "Tạo thêm class CBlock. Block thường là kích thước giống nhau phải không?"
    This reverts commit 251e7c7d444813b789ba6a138987c3bee5114a41.

--- MarioProject/Brick.h
+++ MarioProject/Brick.h
@@ -1,12 +1,15 @@
 #pragma once
-#include "Block.h"
+#include "StaticObject.h"
 
-class CBrick : public CBlock {
+class CBrick : public CStaticObject {
 
 public:
 	CBrick(float x = 0, float y = 0, float vx = 0, float vy = 0, float ax = 0, float ay = 0)
-		: CBlock(x, y, vx, vy, ax, ay) {}
+		: CStaticObject(x, y, vx, vy, ax, ay) {}
 
 	// game object method
 	virtual void Render() override;
+
+	// physical object method
+	virtual void GetBoundingBox(float& left, float& top, float& right, float& bottom) override;
 };
